a38bb4a3cf72b0ec568c41c95ae11f287aba1b09
Kyrie Eleison Varieur <kvarieur@login-i17.local>
2023-02-24 15:45:37 -0800

    adding unit test

--- assignment2_main.py
+++ assignment2_main.py
@@ -96,3 +96,62 @@ class SignalDetection:
 sd = SignalDetection(30, 60, 80, 70)
 sd.plot_roc()
 sd.plot_sdt(sd.d_prime())
+
+import unittest
+from signaldetection import SignalDetection
+
+class TestSignalDetection(unittest.TestCase):
+
+    def test_d_prime_zero(self):
+        sd = SignalDetection(15, 5, 15, 5)
+        expected = 0
+        obtained = sd.d_prime()
+        # Compare calculated and expected d-prime
+        self.assertAlmostEqual(obtained, expected, places=10)
+
+    def test_d_prime_nonzero(self):
+        sd = SignalDetection(15, 10, 15, 5)
+        expected = -0.421142647060282
+        obtained = sd.d_prime()
+        # Compare calculated and expected d-prime
+        self.assertAlmostEqual(obtained, expected, places=10)
+
+    def test_criterion_zero(self):
+        sd = SignalDetection(5, 5, 5, 5)
+        # Calculate expected criterion
+        expected = 0
+        obtained = sd.criterion()
+        # Compare calculated and expected criterion
+        self.assertAlmostEqual(obtained, expected, places=10)
+
+    def test_criterion_nonzero(self):
+        sd = SignalDetection(15, 10, 15, 5)
+        # Calculate expected criterion
+        expected = -0.463918426665941
+        obtained = sd.criterion()
+        # Compare calculated and expected criterion
+        self.assertAlmostEqual(obtained, expected, places=10)
+
+    def test_for_corruption(self):
+        sd = SignalDetection(15, 5, 15, 5)
+        obtained1 = sd.criterion()
+        sd.hits = 9
+        obtained2 = sd.criterion()
+        self.assertNotEqual(obtained1, obtained2)
+
+    def test_addition(self):
+        sd = SignalDetection(1, 1, 2, 1) + SignalDetection(2, 1, 1, 3)
+        expected = SignalDetection(3, 2, 3, 4).criterion()
+        obtained = sd.criterion()
+        # Compare calculated and expected criterion
+        self.assertEqual(obtained, expected)
+
+    def test_multiplication(self):
+        sd = SignalDetection(1, 2, 3, 1) * 4
+        expected = SignalDetection(4, 8, 12, 4).criterion()
+        obtained = sd.criterion()
+        # Compare calculated and expected criterion
+        self.assertEqual(obtained, expected)
+
+if __name__ == '__main__':
+    unittest.main(argv=['first-arg-is-ignored'], exit=False)
